b07d5120904259b03088807f65ceec55f6cdddd1
kevernlt <kev36.rno@gmail.com>
2026-09-23 11:57:54 +0200

    feat: flipping card and check matching

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 //variable d'image
-let dimension = 150;
-let img=[];
+const dimension = 150;
+let img = [];
 
-//variable de jeu
-let firstCard;
+//DOM variables
+const board = document.getElementById("game-board");
+
+//game variables
+let firstCard = null;
 let secondCard;
 let lockBoard = false;
 let moves = 0;
@@ -11,35 +14,77 @@ let matchedCount = 0;
 
 //URL generator
 for (let i = 0; i < 8; i++) {
-    const imgStart = Math.floor(Math.random()*100)+1;
+    const imgStart = Math.floor(Math.random() * 100) + 1;
     const url = `https://picsum.photos/id/${imgStart}/${dimension}`;
     img.push(url);
 }
-let cards=[...img,...img];
+let cards = [...img, ...img];
 
 //card shuffler
-function shuffle(array){
-    for(let i= array.length-1;i>=1;i--){
-        const j =Math.floor(Math.random()*i);
+function shuffle(array) {
+    for (let i = array.length - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * i);
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-function handleCardClick(card){
-    
+//on-click card
+function handleCardClick(card) {
+    if (card.dataset.flip == "true") {
+        return;
+    } else {
+        if (lockBoard == true) {
+            return;
+        } else {
+            card.dataset.flip = "true";
+            if (firstCard == null) {
+                firstCard = card;
+                card.innerHTML = `<img src=${card.dataset.value}>`;
+            } else {
+                secondCard = card;
+                card.innerHTML = `<img src=${card.dataset.value}>`;
+                lockBoard = true;
+                moves++;
+                checkMatch();
+            }
+        }
+    }
+}
+
+//matching clicked card
+function checkMatch() {
+    if (firstCard.dataset.value == secondCard.dataset.value) {
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        lockBoard = false;
+        firstCard = null;
+        secondCard = null;
+    } else {
+        setTimeout(() => {
+            firstCard.innerHTML = "";
+            secondCard.innerHTML = "";
+            firstCard.dataset.flip = "false";
+            secondCard.dataset.flip = "false";
+            lockBoard = false;
+            firstCard = null;
+            secondCard = null;
+        }, 800);
+
+    }
 }
 
 //game initialisation
-function initGame(){
+function initGame() {
     shuffle(cards);
     cards.forEach((element) => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.dataset.value = element;
-        div.role='button';
-        div.tabIndex='0';
-        div.addEventListener('click',()=>handleCardClick(div));
-        document.getElementById("game-board").appendChild(div);
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.dataset.value = element;
+        card.dataset.flip = "false";
+        card.role = 'button';
+        card.tabIndex = '0';
+        card.addEventListener('click', () => handleCardClick(card));
+        board.appendChild(card);
     })
 }
 
